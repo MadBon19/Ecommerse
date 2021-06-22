1b4ac958b99d1bafa2281b1fe9682b3cdc7c79ac
Mona Lisa <de.madbon@gmail.com>
2021-06-22 22:55:11 +0300

    work on basic files in admin branch

---
+++ admin/includes/templates/footer.php
@@ -0,0 +1,10 @@
+
+		<div>
+			This is Footer
+		</div>
+		<script type="text/javascript" src="layout/js/jquery-3.6.0.min.js"></script>
+		<script type="text/javascript" src="layout/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="layout/js/backend.js"></script>
+	</body>
+</html>
+
---
+++ admin/includes/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Admin</title>
+		<link rel="stylesheet" type="text/css" href="layout/css/bootstrap.min.css">
+		<script src="https://kit.fontawesome.com/63c789a859.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" type="text/css" href="layout/css/backend.css">
+	</head>
+	<body>
